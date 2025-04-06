e4a3e24ffed8a123fe5130d3ccdf2695c7842375
abdis <bereketabdisahiev@gmail.com>
2025-04-06 21:03:16 +0500

    render server

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -137,6 +137,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
