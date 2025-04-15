3d4d79f36a22d531f9c2a20ce422bc42e289a346
abdis <bereketabdisahiev@gmail.com>
2025-04-16 04:38:51 +0500

    end

--- taskmaster/wsgi.py
+++ taskmaster/wsgi.py
@@ -14,11 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmaster.settings')
 
 
-# Graphviz жолын қосу
-graphviz_path = "C:\\Program Files\\Graphviz\\bin"
-os.environ["PATH"] += os.pathsep + graphviz_path
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
 application = get_wsgi_application()
 
